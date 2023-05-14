4231c1907651e9847348418280a7f56e6453cf6c
Mariana020221 <mtinajera020221@gmail.com>
2023-05-14 04:23:03 -0600

    iconos y act buscador

--- MAARFOOD/src/app/listas/listas.component.ts
+++ MAARFOOD/src/app/listas/listas.component.ts
@@ -12,18 +12,18 @@ export class ListasComponent implements OnInit {
   reservations: any[] = [];
   searchTerm: string = '';
   showRow: boolean[] = [];
-  category: string = '';
+  category: string = ''; // Se define una variable para almacenar la categoría de búsqueda
 
   constructor(private searchService: SearchService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.reservations = this.searchService.getReservations();
     this.showRow = Array(this.reservations.length).fill(false);
-    this.route.params.subscribe(params => {
-      this.category = params['category'];
-      if (this.category) {
-        this.searchTerm = this.category;
-        this.search();
+    this.route.params.subscribe(params => { // Se suscribe al cambio de parámetros de la ruta
+      this.category = params['category']; // Se obtiene la categoría de búsqueda de los parámetros de la ruta
+      if (this.category) { // Si existe una categoría de búsqueda
+        this.searchTerm = this.category; // Se asigna como término de búsqueda
+        this.search(); // Se realiza la búsqueda
       }
     });
   }
